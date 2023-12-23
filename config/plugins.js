fea0594613a47a8c461745e7f7ab14b71e9d0579
TomasAldea <tomas.sbd.5@gmail.com>
2023-12-23 12:46:19 +0100

    data parser

---
+++ config/plugins.js
@@ -0,0 +1,33 @@
+/* module.exports = ({
+  env
+}) => ({
+  'transformer': {
+    enabled: true,
+    config: {
+      prefix: '/api/',
+      responseTransforms: {
+        removeAttributesKey: true,
+        removeDataKey: true,
+      }
+    }
+  },
+}); */
+
+module.exports = ({ env }) => ({
+   'transformer': {
+      enabled: true,
+      config: {
+        responseTransforms: {
+          removeAttributesKey: true,
+          removeDataKey: true,
+        },
+        requestTransforms : {
+          wrapBodyWithDataKey: false
+        },
+        /* hooks: {
+          preResponseTransform : (ctx) => console.log('hello from the preResponseTransform hook!'),
+          postResponseTransform : (ctx) => console.log('hello from the postResponseTransform hook!')
+        }, */
+      }
+    },
+  });
